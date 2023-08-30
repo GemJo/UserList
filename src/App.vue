386657b30892e79a2c72bc3115b7e6a5795a3911
GemRed <gemjorba@gmail.com>
2023-08-30 17:22:17 +0200

    Created UserView and us it in routes. Created DefaultLayout

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
-<script setup lang="ts">
-import { RouterView } from 'vue-router'
+<template>
+  <component :is="layoutToShow">
+    <RouterView />
+  </component>
+</template>
+<script setup lang='ts'>
+import { RouterView, useRoute } from 'vue-router'
+import { computed } from 'vue';
+import type { Component } from 'vue';
+import DefaultLayout from '@/views/layouts/DefaultLayout.vue';
+
+
+const layouts: {[key: string]: Component} = {
+   default: DefaultLayout,
+};
+
+const layoutToShow = computed<Component>(() => {
+  if (!useRoute().meta.layout) {
+    return layouts['default'];
+  }
+
+  return layouts[useRoute().meta.layout as string];
+})
 </script>
 
-<template>
-  <RouterView />
-</template>
 
 <style scoped>
 
---
+++ src/views/UserView.vue
@@ -0,0 +1,5 @@
+<template>
+</template>
+<script setup lang="ts">
+import UserComponent from "@/components/User/UserComponent.vue";
+</script>

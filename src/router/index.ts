f380580a59e3592463a44bb8a757c6310b8a70e9
GemRed <gemjorba@gmail.com>
2023-08-31 06:41:02 +0200

    Changed route "/" to "/user" and added "/" route alias

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,14 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/user',
       name: 'user',
       component: () => import('@/views/UserView.vue'),
       meta: {
         layout: 'default',
         title: 'User list',
       },
+      alias: "/"
     },
   ]
 });
